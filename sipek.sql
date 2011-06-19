824845de09a4d16986f326e3146ff67779b52294
Mamatia <introvert66.3@gmail.com>
2011-06-20 00:55:21 +0700

    Ubah schema

--- sipek.sql
+++ sipek.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 15, 2011 at 02:23 AM
+-- Generation Time: Jun 20, 2011 at 01:54 AM
 -- Server version: 5.0.45
 -- PHP Version: 5.2.4
 
@@ -24,14 +24,17 @@ CREATE TABLE `anggaran` (
   `sumber` varchar(255) collate latin1_general_ci NOT NULL,
   `jumlah` double NOT NULL,
   `tanggal_terima` date NOT NULL,
-  `periode` year(4) NOT NULL,
+  `periode_id` int(11) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `anggaran`
 --
 
+INSERT INTO `anggaran` (`id`, `sumber`, `jumlah`, `tanggal_terima`, `periode_id`) VALUES
+(1, 'fsdgsgs', 4342525, '2011-05-16', 1),
+(2, 'ghgjghjjjh', 689898988, '2011-05-16', 1);
 
 -- --------------------------------------------------------
 
@@ -42,8 +45,8 @@ CREATE TABLE `anggaran` (
 CREATE TABLE `bobot` (
   `kriteria_id` int(11) NOT NULL,
   `kriteria_pembanding_id` int(11) NOT NULL,
-  `nilai` int(11) NOT NULL,
-  `periode` year(4) NOT NULL,
+  `nilai` double default NULL,
+  `periode_id` int(11) NOT NULL,
   PRIMARY KEY  (`kriteria_id`,`kriteria_pembanding_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
@@ -51,6 +54,45 @@ CREATE TABLE `bobot` (
 -- Dumping data for table `bobot`
 --
 
+INSERT INTO `bobot` (`kriteria_id`, `kriteria_pembanding_id`, `nilai`, `periode_id`) VALUES
+(1, 1, 9, 1),
+(1, 2, 9, 1),
+(1, 3, 9, 1),
+(1, 4, 9, 1),
+(2, 1, 9, 1),
+(2, 2, 9, 1),
+(2, 3, 9, 1),
+(2, 4, 9, 1),
+(3, 1, 9, 1),
+(3, 2, 9, 1),
+(3, 3, 9, 1),
+(3, 4, 9, 1),
+(4, 1, 9, 1),
+(4, 2, 9, 1),
+(4, 3, 9, 1),
+(4, 4, 9, 1),
+(5, 5, 9, 1),
+(5, 6, 9, 1),
+(5, 7, 9, 1),
+(6, 5, 9, 1),
+(6, 6, 9, 1),
+(6, 7, 9, 1),
+(7, 5, 9, 1),
+(7, 6, 9, 1),
+(7, 7, 9, 1),
+(8, 8, 9, 1),
+(8, 9, 9, 1),
+(8, 10, 9, 1),
+(9, 8, 9, 1),
+(9, 9, 9, 1),
+(9, 10, 9, 1),
+(10, 8, 9, 1),
+(10, 9, 9, 1),
+(10, 10, 9, 1),
+(11, 11, 9, 1),
+(11, 12, 9, 1),
+(12, 11, 9, 1),
+(12, 12, 9, 1);
 
 -- --------------------------------------------------------
 
@@ -63,14 +105,16 @@ CREATE TABLE `inventaris` (
   `jenis` varchar(255) collate latin1_general_ci NOT NULL,
   `jumlah` int(11) NOT NULL,
   `kondisi` varchar(255) collate latin1_general_ci NOT NULL,
-  `periode` year(4) NOT NULL,
+  `periode_id` int(11) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `inventaris`
 --
 
+INSERT INTO `inventaris` (`id`, `jenis`, `jumlah`, `kondisi`, `periode_id`) VALUES
+(1, 'bbfdbfb', 45, 'Baik', 1);
 
 -- --------------------------------------------------------
 
@@ -82,12 +126,18 @@ CREATE TABLE `jenis_pelayanan` (
   `id` int(11) NOT NULL auto_increment,
   `nama` varchar(255) collate latin1_general_ci NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `jenis_pelayanan`
 --
 
+INSERT INTO `jenis_pelayanan` (`id`, `nama`) VALUES
+(1, 'Perencanaan, pengendalian, dan pembangunan daerah'),
+(2, 'Pengembangan komunikasi, informasi, media massa, dan pemanfaatan TI'),
+(3, 'Peningkatan kapasitas sumber daya aparatur'),
+(4, 'Peningkatan sarana dan prasarana aparatur'),
+(5, 'Pemeliharaan sarana dan prasarana aparatur');
 
 -- --------------------------------------------------------
 
@@ -98,15 +148,28 @@ CREATE TABLE `jenis_pelayanan` (
 CREATE TABLE `kriteria` (
   `id` int(11) NOT NULL auto_increment,
   `nama` varchar(255) collate latin1_general_ci NOT NULL,
-  `parent_id` int(11) NOT NULL,
+  `parent_id` int(11) default NULL,
   PRIMARY KEY  (`id`),
   KEY `parent_id` (`parent_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=13 ;
 
 --
 -- Dumping data for table `kriteria`
 --
 
+INSERT INTO `kriteria` (`id`, `nama`, `parent_id`) VALUES
+(1, 'Finansial', NULL),
+(2, 'Pelanggan', NULL),
+(3, 'Internal Bisnis', NULL),
+(4, 'Pembelajaran dan Pertumbuhan', NULL),
+(5, 'Anggaran Masuk', 1),
+(6, 'Penggunaan Anggaran', 1),
+(7, 'Efektivitas Anggaran', 1),
+(8, 'Kepuasan Kerja Pegawai', 3),
+(9, 'Peningkatan Kualitas Pegawai', 3),
+(10, 'Kualitas Sarana Prasaran', 3),
+(11, 'Kualitas SDM', 4),
+(12, 'Efektivitas Komunikasi', 4);
 
 -- --------------------------------------------------------
 
@@ -121,15 +184,19 @@ CREATE TABLE `pegawai` (
   `jabatan` varchar(255) collate latin1_general_ci NOT NULL,
   `golongan` varchar(255) collate latin1_general_ci NOT NULL,
   `pend_formal` varchar(255) collate latin1_general_ci NOT NULL,
-  `pend_struktural` varchar(255) collate latin1_general_ci NOT NULL,
-  `periode` year(4) NOT NULL,
+  `status` varchar(255) collate latin1_general_ci NOT NULL,
+  `periode_id` int(11) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `pegawai`
 --
 
+INSERT INTO `pegawai` (`id`, `nip`, `nama`, `jabatan`, `golongan`, `pend_formal`, `status`, `periode_id`) VALUES
+(1, '123456', 'c', 'c', 'II A', 'C', 'C', 0),
+(2, 'a', 'a', 'a', 'II A', 'a', 'a', 0),
+(3, '345677', 'fff', 'fff', 'II A', 'ggggg', 'gggg', 1);
 
 -- --------------------------------------------------------
 
@@ -160,12 +227,15 @@ CREATE TABLE `periode` (
   `id` int(11) NOT NULL auto_increment,
   `nama` year(4) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `periode`
 --
 
+INSERT INTO `periode` (`id`, `nama`) VALUES
+(1, 2010),
+(2, 2011);
 
 -- --------------------------------------------------------
 
@@ -187,6 +257,8 @@ CREATE TABLE `presensi` (
 -- Dumping data for table `presensi`
 --
 
+INSERT INTO `presensi` (`pegawai_id`, `periode_id`, `jumlah_hadir`, `jumlah_sakit`, `jumlah_izin`, `jumlah_tanpa_keterangan`) VALUES
+(0, 1, 333, 2, 4, 4);
 
 -- --------------------------------------------------------
 
@@ -206,10 +278,33 @@ CREATE TABLE `program_kerja` (
   `periode_id` int(11) NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `jenis_pelayanan_id` (`jenis_pelayanan_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `program_kerja`
+--
+
+INSERT INTO `program_kerja` (`id`, `nama`, `jenis_pelayanan_id`, `target_anggaran`, `tanggal_mulai`, `tanggal_target_selesai`, `riil_tanggal_selesai`, `riil_anggaran`, `periode_id`) VALUES
+(1, 'sfskgks', 0, 3940000, '2011-06-20', '2011-06-20', '2011-06-20', 4200000, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sasaran`
+--
+
+CREATE TABLE `sasaran` (
+  `id` int(11) NOT NULL auto_increment,
+  `periode_id` int(11) NOT NULL,
+  `cash_in` int(11) NOT NULL,
+  `cash_out` int(11) NOT NULL,
+  `target_anggaran` int(11) NOT NULL,
+  `target_penggunaan_anggaran` int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `sasaran`
 --
 
 
@@ -232,5 +327,5 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `is_admin`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1),
-(2, 'pengguna', '8b097b8a86f9d6a991357d40d3d58634', 0);
+(1, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 1),
+(2, 'admin', '21232f297a57a5a743894a0e4a801fc3', 0);
