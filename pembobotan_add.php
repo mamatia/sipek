74a69ce6280a56eb82438819eb26aeae02d5bc59
Ahmad Tanwir <th3crypt@gmail.com>
2011-06-19 20:43:33 +0700

    pembobotan_add.php finish

--- pembobotan_add.php
+++ pembobotan_add.php
@@ -2,16 +2,20 @@
 session_start();
 include 'connect.php';
 if(!isset($_SESSION['user'])){
-    header('Location:login.php');
+	header('Location:login.php');
 }
 
 if($_SESSION['user']['is_admin'] == '1'){
-    header('Location:login.php');
+	header('Location:login.php');
 }
 
 $sth = $db->prepare("SELECT * FROM kriteria WHERE parent_id is NULL");
 $sth->execute();
 $listKriteria = $sth->fetchAll (PDO::FETCH_ASSOC);
+
+$sth = $db->prepare("SELECT id,parent_id FROM kriteria");
+$sth->execute();
+$listAllKriteria = $sth->fetchAll(PDO::FETCH_ASSOC);
 
 if(isset($_POST['submit'])) {
 	$db->beginTransaction();
@@ -21,11 +25,19 @@ if(isset($_POST['submit'])) {
 	foreach($_POST['bobot'] as $kriteria_id => $kriteria){
 		foreach($kriteria as $pembanding_id => $nilai){
 			$sth = $db->prepare("INSERT INTO bobot(
-                                        periode_id, kriteria_id, kriteria_pembanding_id, nilai)
-                                    values(?, ?, ?, ?)");
+										periode_id, kriteria_id, kriteria_pembanding_id, nilai)
+									values(?, ?, ?, ?)");
 			$sth->execute(array($periodeId, $kriteria_id, $pembanding_id, $nilai));
 		}
 	}
+
+	foreach($_POST['bobot_kriteria'] as $kriteria_id => $nilai){
+		$sth = $db->prepare("INSERT INTO bobot_kriteria(
+										periode_id, kriteria_id, nilai)
+									values(?, ?, ?)");
+		$sth->execute(array($periodeId, $kriteria_id, $nilai));
+	}
+
 	$saved=true;
 	$db->commit();
 }
@@ -33,7 +45,7 @@ if(isset($_POST['submit'])) {
 
 <?php include 'header.php'?>
 <div class="body" align="center">
-    <h1 align="center">Pembobotan Kriteria Balance Scorecard</h1>
+	<h1 align="center">Pembobotan Kriteria Balance Scorecard</h1>
 	<?php if(isset($saved)):?>
 	<div class="message">
 		Data Tersimpan!
@@ -68,7 +80,7 @@ if(isset($_POST['submit'])) {
 					<td><?php echo $baris['nama'] ?></td>
 					<?php foreach($listKriteria as $kolom):?>
 					<td>
-						<select name="bobot[<?php echo $baris['id'] ?>][<?php echo $kolom['id'] ?>]" id="bobot_<?php echo $baris['id']?>_<?php echo $kolom['id']?>">
+						<select <?php echo $baris['id'] == $kolom['id']?'disabled="disabled"':''?> class="bobot_<?php echo $baris['id']?>" name="bobot[<?php echo $baris['id'] ?>][<?php echo $kolom['id'] ?>]" id="bobot_<?php echo $baris['id']?>_<?php echo $kolom['id']?>">
 							<option value="9">9</option>
 							<option value="8">8</option>
 							<option value="7">7</option>
@@ -78,21 +90,26 @@ if(isset($_POST['submit'])) {
 							<option value="3">3</option>
 							<option value="2">2</option>
 							<option value="1" selected="selected">1</option>
-							<option value="0.50">1/2</option>
-							<option value="0.34">1/3</option>
+							<option value="0.5">1/2</option>
+							<option value="0.33">1/3</option>
 							<option value="0.25">1/4</option>
-							<option value="0.20">1/5</option>
+							<option value="0.2">1/5</option>
 							<option value="0.17">1/6</option>
 							<option value="0.14">1/7</option>
-							<option value="0.12">1/8</option>
+							<option value="0.13">1/8</option>
 							<option value="0.11">1/9</option>
 						</select>
 					</td>
 					<?php endforeach ?>
-					<td id="jumlah_<?php echo $baris['id']?>">-</td>
-					<td id="bobot_<?php echo $baris['id']?>">-</td>
+					<td id="jumlah_<?php echo $baris['id']?>" class="jumlah_<?php echo $baris['id']?>"><?php echo count($listKriteria) ?></td>
+					<td id="bobot_kriteria_display_<?php echo $baris['id']?>" class="bobot_kriteria_<?php echo $baris['id']?>"> <?php echo round(1 / count($listKriteria),2)?></td>
+					<input  id="bobot_kriteria_<?php echo $baris['id']?>" type="hidden" name="bobot_kriteria[<?php echo $baris['id']?>]" value="<?php echo round(1 / count($listKriteria),2)?>"/>
 				</tr>
 				<?php endforeach ?>
+				<tr>
+					<td style="text-align:center; font-weight:bold" colspan="<?php echo count($listKriteria) + 1?>"> <b>Total</b></td>
+					<td id="total_0" style="font-weight:bold"><?php echo pow(count($listKriteria),2) ?></td>
+				</tr>
 			</table>
 			<br/>
 			<?php foreach($listKriteria as $parent):?>
@@ -120,42 +137,98 @@ if(isset($_POST['submit'])) {
 						<td><?php echo $baris['nama'] ?></td>
 						<?php foreach($listChild as $kolom):?>
 						<td>
-							<select name="bobot[<?php echo $baris['id'] ?>][<?php echo $kolom['id'] ?>]" id="bobot_<?php echo $baris['id']?>_<?php echo $kolom['id']?>">
-							<option value="9">9</option>
-							<option value="8">8</option>
-							<option value="7">7</option>
-							<option value="6">6</option>
-							<option value="5">5</option>
-							<option value="4">4</option>
-							<option value="3">3</option>
-							<option value="2">2</option>
-							<option value="1" selected="selected">1</option>
-							<option value="0.50">1/2</option>
-							<option value="0.34">1/3</option>
-							<option value="0.25">1/4</option>
-							<option value="0.20">1/5</option>
-							<option value="0.17">1/6</option>
-							<option value="0.14">1/7</option>
-							<option value="0.12">1/8</option>
-							<option value="0.11">1/9</option>
-							</select>
+							<select <?php echo $baris['id'] == $kolom['id']?'disabled="disabled"':''?> class="bobot_<?php echo $baris['id']?>" name="bobot[<?php echo $baris['id'] ?>][<?php echo $kolom['id'] ?>]" id="bobot_<?php echo $baris['id']?>_<?php echo $kolom['id']?>">
+								<option value="9">9</option>
+								<option value="8">8</option>
+								<option value="7">7</option>
+								<option value="6">6</option>
+								<option value="5">5</option>
+								<option value="4">4</option>
+								<option value="3">3</option>
+								<option value="2">2</option>
+								<option value="1" selected="selected">1</option>
+								<option value="0.5">1/2</option>
+								<option value="0.33">1/3</option>
+								<option value="0.25">1/4</option>
+								<option value="0.2">1/5</option>
+								<option value="0.17">1/6</option>
+								<option value="0.14">1/7</option>
+								<option value="0.13">1/8</option>
+								<option value="0.11">1/9</option>
+								</select>
 						</td>
 						<?php endforeach ?>
-						<td id="jumlah_<?php echo $baris['id']?>">-</td>
-						<td id="bobot_<?php echo $baris['id']?>">-</td>
+						<td id="jumlah_<?php echo $baris['id']?>" class="jumlah_<?php echo $baris['id']?>"><?php echo count($listChild) ?></td>
+						<td id="bobot_kriteria_display_<?php echo $baris['id']?>" class="bobot_kriteria_<?php echo $baris['id']?>"> <?php echo round(1 / count($listChild),2)?></td>
+						<input  id="bobot_kriteria_<?php echo $baris['id']?>" type="hidden" name="bobot_kriteria[<?php echo $baris['id']?>]" value="<?php echo round(1 / count($listChild),2)?>"/>
 					</tr>
 					<?php endforeach ?>
+					<tr>
+						<td style="text-align:center; font-weight:bold" colspan="<?php echo count($listChild) + 1?>"> <b>Total</b></td>
+						<td id="total_<?php echo $parent['id'] ?>" style="font-weight:bold"><?php echo pow(count($listChild),2) ?></td>
+					</tr>
 				</table>
 			</div>
 			<?php endif ?>
 			<?php endforeach ?>
 			<div class="buttons" align="center">
-	            <span class="button"><input class="save" name="submit" type="submit" value="Create" /></span>
-	        </div>
+				<span class="button"><input class="save" name="submit" type="submit" value="Create" /></span>
+			</div>
 		</div>
 	</form>
 </div>
 <script type="text/javascript">
-	window.addEvent()
+
+Number.implement({
+	'round': function(dec){
+		return Math.round(this * Math.pow(10,dec))/Math.pow(10,dec);
+	}
+});
+
+var hitung_jumlah = function(kriteria_id, parent_id) {
+	document.id('jumlah_' + kriteria_id).set('html',(function(){
+		var old_jumlah = document.id('jumlah_' + kriteria_id).get('html').toFloat();
+		var new_jumlah = 0;
+		$$('.bobot_' + kriteria_id).each(function(e_bobot){
+			new_jumlah += e_bobot.get('value').toFloat();
+		});
+		new_jumlah = new_jumlah.round(2);
+		document.id('total_' + parent_id).set('html', (function(){
+			var total = document.id('total_' + parent_id).get('html').toFloat() - old_jumlah + new_jumlah;
+			document.id('bobot_kriteria_' + kriteria_id).set('value', (new_jumlah / total).round(2));
+			document.id('bobot_kriteria_display_' + kriteria_id).set('html', (new_jumlah / total).round(2));
+			return total.round(2);
+		})());
+		return new_jumlah;
+	})());
+}
+
+
+var hitung_ahp = function(kriteria_id, parent_id) {
+	parent_id = parent_id == null ? 0 : parent_id;
+	$$('.bobot_' + kriteria_id).each(function(eselect, index){
+		eselect.addEvent('change',function(){
+			hitung_jumlah(kriteria_id, parent_id);
+			var arr = eselect.get('id').replace('bobot_','').split('_');
+			var new_id = 'bobot_' + arr[1] + '_' + arr[0];
+			document.id(new_id).set('value',(function(){
+				var nilai = eselect.get('value');
+				if(nilai > 1) {
+					return (1 /  nilai).round(2);
+				}
+				return Math.round(1 / nilai);
+			})());
+			hitung_jumlah(arr[1], parent_id);
+		});
+	});
+};
+var penyesuaian_bobot = function(kriteria_id) {
+
+};
+window.addEvent('domready',function(){
+	<?php echo json_encode($listAllKriteria)?>.each(function(kriteria){
+		hitung_ahp(kriteria['id'], kriteria['parent_id']);
+	});
+});
 </script>
 <?php include 'footer.php' ?>
--- sipek.sql
+++ sipek.sql
@@ -1,13 +1,19 @@
 -- phpMyAdmin SQL Dump
--- version 2.11.1
+-- version 3.3.8.1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 20, 2011 at 01:54 AM
--- Server version: 5.0.45
--- PHP Version: 5.2.4
+-- Generation Time: Jun 19, 2011 at 08:42 PM
+-- Server version: 5.1.51
+-- PHP Version: 5.3.6-pl0-gentoo
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `sipek`
@@ -19,14 +25,14 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Table structure for table `anggaran`
 --
 
-CREATE TABLE `anggaran` (
-  `id` int(11) NOT NULL auto_increment,
-  `sumber` varchar(255) collate latin1_general_ci NOT NULL,
+CREATE TABLE IF NOT EXISTS `anggaran` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sumber` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `jumlah` double NOT NULL,
   `tanggal_terima` date NOT NULL,
   `periode_id` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=3 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `anggaran`
@@ -42,57 +48,77 @@ INSERT INTO `anggaran` (`id`, `sumber`, `jumlah`, `tanggal_terima`, `periode_id`
 -- Table structure for table `bobot`
 --
 
-CREATE TABLE `bobot` (
+CREATE TABLE IF NOT EXISTS `bobot` (
   `kriteria_id` int(11) NOT NULL,
   `kriteria_pembanding_id` int(11) NOT NULL,
-  `nilai` double default NULL,
+  `nilai` double DEFAULT NULL,
   `periode_id` int(11) NOT NULL,
-  PRIMARY KEY  (`kriteria_id`,`kriteria_pembanding_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+  PRIMARY KEY (`kriteria_id`,`kriteria_pembanding_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
 -- Dumping data for table `bobot`
 --
 
 INSERT INTO `bobot` (`kriteria_id`, `kriteria_pembanding_id`, `nilai`, `periode_id`) VALUES
-(1, 1, 9, 1),
-(1, 2, 9, 1),
-(1, 3, 9, 1),
-(1, 4, 9, 1),
-(2, 1, 9, 1),
-(2, 2, 9, 1),
-(2, 3, 9, 1),
-(2, 4, 9, 1),
-(3, 1, 9, 1),
-(3, 2, 9, 1),
-(3, 3, 9, 1),
-(3, 4, 9, 1),
-(4, 1, 9, 1),
-(4, 2, 9, 1),
-(4, 3, 9, 1),
-(4, 4, 9, 1),
-(5, 5, 9, 1),
-(5, 6, 9, 1),
-(5, 7, 9, 1),
-(6, 5, 9, 1),
-(6, 6, 9, 1),
-(6, 7, 9, 1),
-(7, 5, 9, 1),
-(7, 6, 9, 1),
-(7, 7, 9, 1),
-(8, 8, 9, 1),
-(8, 9, 9, 1),
-(8, 10, 9, 1),
-(9, 8, 9, 1),
-(9, 9, 9, 1),
-(9, 10, 9, 1),
-(10, 8, 9, 1),
-(10, 9, 9, 1),
-(10, 10, 9, 1),
-(11, 11, 9, 1),
-(11, 12, 9, 1),
-(12, 11, 9, 1),
-(12, 12, 9, 1);
+(1, 2, 1, 1),
+(1, 3, 0.5, 1),
+(1, 4, 2, 1),
+(2, 1, 1, 1),
+(2, 3, 0.5, 1),
+(2, 4, 1, 1),
+(3, 1, 2, 1),
+(3, 2, 2, 1),
+(3, 4, 1, 1),
+(4, 1, 0.5, 1),
+(4, 2, 1, 1),
+(4, 3, 1, 1),
+(5, 6, 6, 1),
+(5, 7, 5, 1),
+(6, 5, 0.17, 1),
+(6, 7, 6, 1),
+(7, 5, 0.2, 1),
+(7, 6, 0.17, 1),
+(8, 9, 5, 1),
+(8, 10, 4, 1),
+(9, 8, 0.2, 1),
+(9, 10, 0.2, 1),
+(10, 8, 0.25, 1),
+(10, 9, 5, 1),
+(11, 12, 0.14, 1),
+(12, 11, 7, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bobot_kriteria`
+--
+
+CREATE TABLE IF NOT EXISTS `bobot_kriteria` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kriteria_id` int(11) NOT NULL,
+  `periode_id` int(11) NOT NULL,
+  `nilai` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+
+--
+-- Dumping data for table `bobot_kriteria`
+--
+
+INSERT INTO `bobot_kriteria` (`id`, `kriteria_id`, `periode_id`, `nilai`) VALUES
+(1, 1, 1, 0.22),
+(2, 2, 1, 0.19),
+(3, 3, 1, 0.34),
+(4, 4, 1, 0.21),
+(5, 5, 1, 0.7),
+(6, 6, 1, 0.34),
+(7, 7, 1, 0.07),
+(8, 8, 1, 0.66),
+(9, 9, 1, 0.08),
+(10, 10, 1, 0.34),
+(11, 11, 1, 0.12),
+(12, 12, 1, 0.8);
 
 -- --------------------------------------------------------
 
@@ -100,14 +126,14 @@ INSERT INTO `bobot` (`kriteria_id`, `kriteria_pembanding_id`, `nilai`, `periode_
 -- Table structure for table `inventaris`
 --
 
-CREATE TABLE `inventaris` (
-  `id` int(11) NOT NULL auto_increment,
-  `jenis` varchar(255) collate latin1_general_ci NOT NULL,
+CREATE TABLE IF NOT EXISTS `inventaris` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `jenis` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `jumlah` int(11) NOT NULL,
-  `kondisi` varchar(255) collate latin1_general_ci NOT NULL,
+  `kondisi` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `periode_id` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=2 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `inventaris`
@@ -122,11 +148,11 @@ INSERT INTO `inventaris` (`id`, `jenis`, `jumlah`, `kondisi`, `periode_id`) VALU
 -- Table structure for table `jenis_pelayanan`
 --
 
-CREATE TABLE `jenis_pelayanan` (
-  `id` int(11) NOT NULL auto_increment,
-  `nama` varchar(255) collate latin1_general_ci NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=6 ;
+CREATE TABLE IF NOT EXISTS `jenis_pelayanan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) COLLATE latin1_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `jenis_pelayanan`
@@ -145,13 +171,13 @@ INSERT INTO `jenis_pelayanan` (`id`, `nama`) VALUES
 -- Table structure for table `kriteria`
 --
 
-CREATE TABLE `kriteria` (
-  `id` int(11) NOT NULL auto_increment,
-  `nama` varchar(255) collate latin1_general_ci NOT NULL,
-  `parent_id` int(11) default NULL,
-  PRIMARY KEY  (`id`),
+CREATE TABLE IF NOT EXISTS `kriteria` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) COLLATE latin1_general_ci NOT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `parent_id` (`parent_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=13 ;
 
 --
 -- Dumping data for table `kriteria`
@@ -177,17 +203,17 @@ INSERT INTO `kriteria` (`id`, `nama`, `parent_id`) VALUES
 -- Table structure for table `pegawai`
 --
 
-CREATE TABLE `pegawai` (
-  `id` int(11) NOT NULL auto_increment,
-  `nip` varchar(255) collate latin1_general_ci NOT NULL,
-  `nama` varchar(255) collate latin1_general_ci NOT NULL,
-  `jabatan` varchar(255) collate latin1_general_ci NOT NULL,
-  `golongan` varchar(255) collate latin1_general_ci NOT NULL,
-  `pend_formal` varchar(255) collate latin1_general_ci NOT NULL,
-  `status` varchar(255) collate latin1_general_ci NOT NULL,
+CREATE TABLE IF NOT EXISTS `pegawai` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nip` varchar(255) COLLATE latin1_general_ci NOT NULL,
+  `nama` varchar(255) COLLATE latin1_general_ci NOT NULL,
+  `jabatan` varchar(255) COLLATE latin1_general_ci NOT NULL,
+  `golongan` varchar(255) COLLATE latin1_general_ci NOT NULL,
+  `pend_formal` varchar(255) COLLATE latin1_general_ci NOT NULL,
+  `status` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `periode_id` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=4 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `pegawai`
@@ -204,13 +230,13 @@ INSERT INTO `pegawai` (`id`, `nip`, `nama`, `jabatan`, `golongan`, `pend_formal`
 -- Table structure for table `penugasan`
 --
 
-CREATE TABLE `penugasan` (
+CREATE TABLE IF NOT EXISTS `penugasan` (
   `pegawai_id` int(11) NOT NULL,
-  `nama_penugasan` varchar(250) collate latin1_general_ci NOT NULL,
-  `tingkat` varchar(250) collate latin1_general_ci NOT NULL,
+  `nama_penugasan` varchar(250) COLLATE latin1_general_ci NOT NULL,
+  `tingkat` varchar(250) COLLATE latin1_general_ci NOT NULL,
   `periode_id` int(11) NOT NULL,
-  PRIMARY KEY  (`pegawai_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+  PRIMARY KEY (`pegawai_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
 -- Dumping data for table `penugasan`
@@ -223,19 +249,18 @@ CREATE TABLE `penugasan` (
 -- Table structure for table `periode`
 --
 
-CREATE TABLE `periode` (
-  `id` int(11) NOT NULL auto_increment,
+CREATE TABLE IF NOT EXISTS `periode` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nama` year(4) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=3 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `periode`
 --
 
 INSERT INTO `periode` (`id`, `nama`) VALUES
-(1, 2010),
-(2, 2011);
+(1, 2010);
 
 -- --------------------------------------------------------
 
@@ -243,15 +268,15 @@ INSERT INTO `periode` (`id`, `nama`) VALUES
 -- Table structure for table `presensi`
 --
 
-CREATE TABLE `presensi` (
+CREATE TABLE IF NOT EXISTS `presensi` (
   `pegawai_id` int(11) NOT NULL,
   `periode_id` int(11) NOT NULL,
   `jumlah_hadir` int(11) NOT NULL,
   `jumlah_sakit` int(11) NOT NULL,
   `jumlah_izin` int(11) NOT NULL,
   `jumlah_tanpa_keterangan` int(11) NOT NULL,
-  PRIMARY KEY  (`pegawai_id`,`periode_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+  PRIMARY KEY (`pegawai_id`,`periode_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
 -- Dumping data for table `presensi`
@@ -266,9 +291,9 @@ INSERT INTO `presensi` (`pegawai_id`, `periode_id`, `jumlah_hadir`, `jumlah_saki
 -- Table structure for table `program_kerja`
 --
 
-CREATE TABLE `program_kerja` (
-  `id` int(11) NOT NULL auto_increment,
-  `nama` varchar(255) collate latin1_general_ci NOT NULL,
+CREATE TABLE IF NOT EXISTS `program_kerja` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `jenis_pelayanan_id` int(11) NOT NULL,
   `target_anggaran` double NOT NULL,
   `tanggal_mulai` date NOT NULL,
@@ -276,9 +301,9 @@ CREATE TABLE `program_kerja` (
   `riil_tanggal_selesai` date NOT NULL,
   `riil_anggaran` double NOT NULL,
   `periode_id` int(11) NOT NULL,
-  PRIMARY KEY  (`id`),
+  PRIMARY KEY (`id`),
   KEY `jenis_pelayanan_id` (`jenis_pelayanan_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `program_kerja`
@@ -293,15 +318,15 @@ INSERT INTO `program_kerja` (`id`, `nama`, `jenis_pelayanan_id`, `target_anggara
 -- Table structure for table `sasaran`
 --
 
-CREATE TABLE `sasaran` (
-  `id` int(11) NOT NULL auto_increment,
+CREATE TABLE IF NOT EXISTS `sasaran` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `periode_id` int(11) NOT NULL,
   `cash_in` int(11) NOT NULL,
   `cash_out` int(11) NOT NULL,
   `target_anggaran` int(11) NOT NULL,
   `target_penggunaan_anggaran` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `sasaran`
@@ -314,18 +339,18 @@ CREATE TABLE `sasaran` (
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL auto_increment,
-  `username` varchar(255) collate latin1_general_ci NOT NULL,
-  `password` varchar(255) collate latin1_general_ci NOT NULL,
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE latin1_general_ci NOT NULL,
+  `password` varchar(255) COLLATE latin1_general_ci NOT NULL,
   `is_admin` tinyint(1) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=3 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `is_admin`) VALUES
-(1, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 1),
-(2, 'admin', '21232f297a57a5a743894a0e4a801fc3', 0);
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1),
+(2, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 0);
